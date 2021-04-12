c18e0756a429c912f63343d456d38803d293d886
Carlos Ramirez <c.ramirezm@uniandes.edu.co>
2021-04-12 14:41:33 -0500

    Taller 1 Completado

---
+++ scripts/dataCourses.js
@@ -0,0 +1,9 @@
+import { Course } from './course.js';
+export var dataCourses = [
+    new Course("Desarrollo de SW por Equpos", "César Forero", 3),
+    new Course("Sistemas Transaccionales", "Claudia Jimenez", 3),
+    new Course("Diseño y Analisis de Algorimos", "Rodrigo Cardozo", 3),
+    new Course("Aleman 3", "Natalia Rincon", 3),
+    new Course("Química", "Jhon Hurtado", 3),
+    new Course("Lab Química", "Ingrid Betancour", 0)
+];
